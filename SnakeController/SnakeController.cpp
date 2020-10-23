be3787b3f8bf9b1dc7f660187416f77ff005240f
pala19 <p.biruta19@gmail.com>
2020-10-23 17:31:05 +0200

    changed to if-else

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -215,22 +215,25 @@ Controller::Segment Controller::getNewHead() const
 
 void Controller::receive(std::unique_ptr<Event> e)
 {
-    try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
-    } catch (std::bad_cast&) {
-        try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
-        } catch (std::bad_cast&) {
-            try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
-            } catch (std::bad_cast&) {
-                try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
-                } catch (std::bad_cast&) {
-                    throw UnexpectedEventException();
-                }
-            }
-        }
+  if (e->getMessageId() == TimeoutInd::MESSAGE_ID)
+  {
+      handleTimePassed(TimeoutInd{});
+  }
+  else if (e->getMessageId() == DirectionInd::MESSAGE_ID)
+   {
+      handleDirectionChange(DirectionInd{});
+   }
+  else if (e->getMessageId() == FoodInd::MESSAGE_ID)
+    {
+      handleFoodPositionChange(FoodInd{});
+    }
+  else if (e->getMessageId() == FoodResp::MESSAGE_ID)
+    {
+      handleNewFood(FoodResp{});
+    }
+  else
+    {
+    throw UnexpectedEventException();
     }
 }
 
